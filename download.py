49d60a0f71a9d9f67750456e95871f6d32525909
Carlo <carlo.danieli@rolls-royce.com>
2025-02-07 10:30:04 +0000

    Add files via upload

---
+++ download.py
@@ -0,0 +1,22 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("namantrisoliya/amazon-skincare-products")
+
+print("Path to dataset files:", path)
+
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("oftomorrow/herokuapp-makeup-products")
+
+print("Path to dataset files:", path)
+
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("abhinavtyagi2708/zara-dataset-men-and-women-clothing")
+
+print("Path to dataset files:", path)
